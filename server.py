7d475b9db3ad03021e29c89e5f35fb4fbcec0c7a
Bottersnike <bottersnike237@gmail.com>
2018-04-04 12:52:53 +0100

    Patch server to remove dual loopage

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import aiomcache
-import uvloop
 
 from sanic import Sanic, response
 from sanic.exceptions import NotFound, FileNotFound
@@ -16,27 +15,28 @@ app = Sanic(__name__)
 pool = DatabasePool()
 app.listener('before_server_start')(pool.register_db)
 
-loop = uvloop.new_event_loop()
-client = aiomcache.Client("127.0.0.1", 11211, loop=loop)
-session_interface = MemcacheSessionInterface(client)
+
+@app.listener('before_server_start')
+async def thing(_, loop):
+    client = aiomcache.Client("127.0.0.1", 11211, loop=loop)
+    session_interface = MemcacheSessionInterface(client)
+
+    @app.middleware('request')
+    async def add_session_to_request(request):
+        await session_interface.open(request)
+
+    @app.middleware('response')
+    async def save_session(request, response):
+        await session_interface.save(request, response)
+
 
 Authentication(pool).register(app)
 ArticleFactory(pool).register(app)
 
 
-@app.middleware('request')
-async def add_session_to_request(request):
-    await session_interface.open(request)
-
-
-@app.middleware('response')
-async def save_session(request, response):
-    await session_interface.save(request, response)
-
-
 @app.exception(NotFound)
 @app.exception(FileNotFound)
-async def not_found(*_):
+async def not_found(*_, **__):
     return await response.file('static/404.html')
 
 
